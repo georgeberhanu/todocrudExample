62975c1fde7411e3de3d49d2cc30d6dae6e305ea
georgeberhanu <georgb5225@gmail.com>
2019-12-17 18:13:15 +0300

    initial commit

---
+++ TodoCrud/Controllers/todoesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TodoCrud;
+
+namespace TodoCrud.Controllers
+{
+    public class todoesController : Controller
+    {
+        private todoEntities db = new todoEntities();
+
+        // GET: todoes
+        public ActionResult Index()
+        {
+            return View(db.todoes.ToList());
+        }
+
+        // GET: todoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            todo todo = db.todoes.Find(id);
+            if (todo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(todo);
+        }
+
+        // GET: todoes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: todoes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,date,description")] todo todo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.todoes.Add(todo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(todo);
+        }
+
+        // GET: todoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            todo todo = db.todoes.Find(id);
+            if (todo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(todo);
+        }
+
+        // POST: todoes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,date,description")] todo todo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(todo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(todo);
+        }
+
+        // GET: todoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            todo todo = db.todoes.Find(id);
+            if (todo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(todo);
+        }
+
+        // POST: todoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            todo todo = db.todoes.Find(id);
+            db.todoes.Remove(todo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
